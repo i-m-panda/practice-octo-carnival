89784fd272a597fbfac0867d8feb0a8bd061ca97
Abhishek Singh <abesingh1@gmail.com>
2024-04-24 11:17:56 +0530

    tuples

---
+++ Python shenanigans/tuples.py
@@ -0,0 +1,15 @@
+# Tuple is a list that is immutable. 
+# In some ways, a tuple is similar to a Python list in terms of indexing,
+# nested objects, and repetition but the main difference between both is Python tuple is immutable
+
+myTuple = (1 ,3 ,5, 2)
+
+print(myTuple[2])
+
+# myTuple[2] = 6 will throw an Error as tuples are immutable
+
+# Some important points regarding immutability
+# - One cannot add items to a tuple once it is created.
+# - Tuples cannot be appended or extended.
+# - We cannot remove items from a tuple once it is created.
+# source: geeksforgeeks
