1f3110ceaa2e7fd5cefb1302a58cc0d5e3444407
Abhishek Singh <abesingh1@gmail.com>
2024-05-15 11:24:52 +0530

    file ops and exceptions

--- Python shenanigans/files_ops_and_exception.py
+++ Python shenanigans/files_ops_and_exception.py
@@ -11,3 +11,59 @@ with open("pi.txt", "r", encoding="utf-8") as file_object:
     # an empty line once it reaches the end of file which shows up as blank line,
     # use rstrip() or strip() to remove it)
     print(content)
+
+# you can use for loop on file_object to read file line by line,
+# remember that each line if printed, will have an implicit blank line
+# consider the code below
+
+with open("alpha.txt", "r", encoding="utf-8") as file_object:
+    for each_lines in file_object:
+        print(each_lines)
+
+# you can store lines as list if you want to access them outside of "with" block
+# or do some manipulation on the read data
+
+with open("alpha.txt", "r", encoding="utf-8") as file_object:
+    lines: list[str] = [line.rstrip() for line in file_object.readlines()]
+
+print(lines)
+
+# different kinds of modes with which files can be opened are: r(for read), w(for write)
+# a(for append), r+(for read/write)
+
+# open() creates a file if doesn't exists
+with open("greetings.txt", "w", encoding="utf-8") as file_object:
+    file_object.write("Hello World!!")
+
+# python can only insert string to a text file, type casting to string is required
+# if you are trying to insert data other than string
+
+# when writing multiple lines to a text file, remember to add new line like \n, \r, etc.
+# as python won't add a new line after writing a line in text file
+
+# EXCEPTIONS
+# exceptions are special objects that python uses to manage errors
+# that arises during program execution
+
+# you can use try-except or try-except-else block to handle exceptions
+# syntax:
+# try:
+    # code block that may produce an error
+# except <EXCEPTION_NAME>:
+    # code block that will run in case of error
+# else:
+    # code block that will only run if try block is executed successfully (else block is optional)
+
+try:
+    result = 5/0
+except ZeroDivisionError:
+    print("You can't divide a number by zero")
+else:
+    # will never be executed in this case, but if the expression in try block was valid,
+    # result would be printed
+    print(result)
+
+# when exception occurs and it's not handled, the program execution halts
+
+# you can use "pass" keyword in except block if you want to program to execute
+# and do nothing in case exception occured
